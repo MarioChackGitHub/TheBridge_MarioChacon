57803ddcc0f435d9848715365061e0de4d33a744
MarioChackGitHub <101821489+MarioChackGitHub@users.noreply.github.com>
2022-04-17 18:55:10 +0200

    Delete Hundir la flota directory

--- Hundir la flota/endofgame.py
+++
@@ -1,8 +0,0 @@
-
-def endgame(tablerojugador, tablerocomputadora):
-    if 'O' in tablerocomputadora:
-        print("Enhorabuena, has derrotado a la computadora!!")
-
-
-    elif 'O' in tablerojugador:
-        print("La computadora te derrotó :`(")
